d0ac058f6573cd55e30dd8df7829d60bf0ccff8d
IsraRamos <116392477+Jisraman@users.noreply.github.com>
2023-12-10 09:18:08 -0600

    first commit

---
+++ src/plugins/element.d.ts
@@ -0,0 +1,8 @@
+// element.d.ts
+declare module './plugins/element' {
+    import ElementPlus from 'element-plus';
+    import 'element-plus/theme-chalk/index.css';
+  
+    export default ElementPlus;
+  }
+  
---
+++ src/plugins/element.ts
@@ -0,0 +1,5 @@
+// element.js
+import ElementPlus from 'element-plus'
+import 'element-plus/theme-chalk/index.css'
+
+export default ElementPlus
