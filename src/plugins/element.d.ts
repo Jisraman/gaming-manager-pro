b2cd90fd9368a39aee8ae3d79855b8c9c646aa94
jisraman <gustavoisraelramosmontes@gmail.com>
2023-12-18 04:20:25 -0600

    test 1

--- src/plugins/element.d.ts
+++
@@ -1,8 +0,0 @@
-// element.d.ts
-declare module './plugins/element' {
-    import ElementPlus from 'element-plus';
-    import 'element-plus/theme-chalk/index.css';
-  
-    export default ElementPlus;
-  }
-  
--- src/plugins/element.ts
+++
@@ -1,5 +0,0 @@
-// element.js
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-
-export default ElementPlus
